df890b91bc63c1baca30e56552cade32f4d36887
kaku993 <redfox2511@gmail.com>
2021-08-24 13:09:20 +0900

    Add files via upload

---
+++ catpage.js
@@ -0,0 +1,60 @@
+
+//スライドショー
+window.addEventListener('load', function () {
+	viewSlide('.slide img');
+});
+function viewSlide(className, slideNo = -1)
+{
+	let imgArray = document.querySelectorAll(className);
+	if (slideNo >= 0) {
+		//初回以外は現在のスライドを消す
+		imgArray[slideNo].style.opacity = 0;
+	}
+	slideNo++;
+	if (slideNo >= imgArray.length) {
+		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
+	}
+	imgArray[slideNo].style.opacity = 1;
+	let msec = document.getElementById('slide_speed').value;
+	setTimeout(function(){viewSlide(className, slideNo);}, msec);
+}
+let msec = document.getElementById('slide_speed').max - document.getElementById('slide_speed').value;
+
+//フッターの時計
+timerID = setInterval('clock()',500); //0.5秒毎にclock()を実行
+
+function clock() {
+	document.getElementById("view_clock").innerHTML = getNow();
+}
+
+function getNow() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１を足すこと
+	var day = now.getDate();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+	var sec = now.getSeconds();
+
+	//出力用
+	var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒"; 
+	return s;
+}
+
+
+function twitText() {
+	var s, url;
+	s = "MEOW!";
+	url = document.location.href;
+	
+	if (s != "") {
+		if (s.length > 140) {
+			//文字数制限
+			alert("テキストが140字を超えています");
+		} else {
+			//投稿画面を開く
+			url = "http://twitter.com/share?url=" + escape(url) + "&text=" + s;
+			window.open(url,"_blank","width=600,height=300");
+		}
+	}
+}
